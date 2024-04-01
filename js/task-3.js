0867276916ec927735099c067e9a2956587f92a5
Olga Blud <olga.blud@gmail.com>
2024-04-01 13:43:09 +0300

    add h1 and use strict

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,4 @@
+'use strict';
 function getElementWidth(content, padding, border) {
   return (
     Number.parseFloat(content) +
@@ -6,6 +7,6 @@ function getElementWidth(content, padding, border) {
   );
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
