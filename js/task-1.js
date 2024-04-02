a7f3096f4309c9511cecf647a41042d050d570c7
Olga Blud <olga.blud@gmail.com>
2024-04-02 13:50:29 +0300

    add basic html

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
